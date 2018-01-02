6ce6f9ab94f613dc5ffb475c09fdd9c39d507ea6
Nic Hannah <nic@doubleprecision.com.au>
2018-01-02 22:42:24 +1100

    Search for forcing data based on date. #8

--- source/atm_forcing.F90
+++ source/atm_forcing.F90
@@ -71,9 +71,11 @@ subroutine get_forcing(key, date, data)
 
     character(len=*), intent(in) :: key
     type(datetime), intent(in) :: date
+    integer, intent(in) :: forcing_year
     real, dimension(:,:), intent(inout) :: data
 
-    character(len=256) :: filename, fieldname
+    character(len=256) :: filename, fieldname, time_str
+    type(datetime), intent(in) :: start_date, start_date_w_hours
     character(len=4) :: year
 
     if (.not. (trim(key) .in. filename_dict)) then
@@ -87,13 +89,84 @@ subroutine get_forcing(key, date, data)
 	print*, 'val: ', trim(fieldname)
 
     ! Find the correct file. First select the year.
-    write(year,'(i4)') date%getYear()
-	filename = replace_text(filename, "{{ year }}", year)
-	filename = replace_text(filename, "{{year}}", year)
+	filename = replace_text(filename, "{{ year }}", forcing_year)
+	filename = replace_text(filename, "{{year}}", forcing_year)
+
+    ! Check that the file exists.
 
     ! Find the correct timeslice in the file.
+    call ncheck(nf90_open(trim(filename), NF90_NOWRITE, ncid), &
+                'Opening '//trim(filename))
+
+    ! Get variable ID
+    call ncheck(nf90_inq_varid(ncid, trim(varname), varid), &
+                'Inquire: '//trim(varname))
+
+    ! Get start date
+    call ncheck(nf90_get_att(ncid, varid, "units", time_str)
+
+    time_str = replace_text(time_str, "days since ", "")
+    start_date_w_hours = strptime(trim(time_str), "%Y-%m-%d %H:%M:%S")
+    start_date = strptime(trim(time_str), "%Y-%m-%d")
+
+    indx = forcing_index(ncid, start_date, date, guess)
+
+    call read_data()
+
 
 end subroutine get_forcing
+
+!> Take a netcdf time attribute and return a date.
+subroutine get_start_date(time_str, date)
+
+    character(len=*), intent(in) :: time_str
+    type(datetime), intent(out) :: date
+
+    date = strptime
+
+end subroutine
+
+!> Return the time index of a particular date.
+! Starts looking from a guess index.
+function forcing_index(ncid, start_date, target_date, guess)  result(indx)
+
+    integer, intent(in) :: ncid, varid
+    type(datetime), intent(in) :: start_date, target_date, tmp_date
+    integer, intent(in) :: guess
+    integer, intent(out) :: indx
+
+    integer :: varid, num_times
+    real, dimension(:), allocatable :: times
+
+    ! Get variable ID
+    call ncheck(nf90_inq_varid(ncid, "time", varid), 
+                'Inquire: '//trim(varname))
+
+    call ncheck(nf90_inquire_dimension(ncid, varid, len = num_times))
+    allocate(times(num_times))
+    call ncheck(nf90_get_var(ncid, varid, times)
+
+    ! First start searching using 'guess'
+    do indx=guess, num_times
+        tmp_date = start_date + times(indx)
+        if (tmp_date == target_date) then
+            return
+        endif
+    enddo
+
+    ! Searching from the beginning
+    do indx=1, num_times
+        tmp_date = start_date + times(indx)
+        if (tmp_date == target_date) then
+            return
+        endif
+    enddo
+
+    ! The date was not found.
+    indx = -1
+
+end function forcing_index()
+
 
 !> Replace all occurrences of 'pattern' with 'replace' in string.
 ! Based on: http://fortranwiki.org/fortran/show/String_Functions
@@ -110,5 +183,24 @@ function replace_text(string, pattern, replace)  result(outs)
 	end do
 
 end function replace_text
+
+subroutine ncheck(status, error_str)
+
+    implicit none
+
+    integer(kind=int_kind), intent(in) :: status
+    character(len=*), intent(in), optional :: error_str
+
+    if (status /= nf90_noerr) then
+      write(*,'(/a)')   'MATM: error - from NetCDF library'
+    if (present(error_str)) then
+        write(*,'(a)')   error_str
+      endif
+      write(*,'(a/)')   trim(nf90_strerror(status))
+      stop
+    end if
+
+end subroutine ncheck
+
 
 end module atm_forcing
