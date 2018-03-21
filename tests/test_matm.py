a2edf4c606e7629cc69bb687a73b6423afaeb6ec
Nic Hannah <nic@doubleprecision.com.au>
2018-03-21 15:39:16 +1100

    Set up test directories.

---
+++ tests/test_matm.py
@@ -0,0 +1,22 @@
+
+import pytest
+
+class TestAtm:
+
+    def test_forcing_checksums():
+        """
+        Test that atmospheric forcing checksums have not changed.
+        """
+        pass
+
+    def test_timestamps():
+        """
+        Check that coupling timestamps are as expected.
+        """
+        pass
+
+    def test_restart():
+        """
+        Test that model restarts at the correct date.
+        """
+        pass
