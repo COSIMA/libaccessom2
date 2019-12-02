17bed6ff58b3f28d098913aaca3ce37753c6c7d1
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2019-12-02 11:39:55 +1100

    Add gadi build script

---
+++ build_on_gadi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge
+module load intel-compiler/2019.5.281
+module load netcdf/4.7.1
+module load openmpi/4.0.1
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Args: platform and whether Cmake should search for NetCDF libraries
+source $MYDIR/build.sh nci OFF
