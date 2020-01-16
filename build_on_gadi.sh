575fb04771e5442e19654c27b183e92d8b205f3f
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-01-17 09:56:58 +1100

    Use OpenMPI/4.0.2 - https://github.com/COSIMA/access-om2/issues/157

--- build_on_gadi.sh
+++ build_on_gadi.sh
@@ -3,7 +3,7 @@
 module purge
 module load intel-compiler/2019.5.281
 module load netcdf/4.7.1
-module load openmpi/4.0.1
+module load openmpi/4.0.2
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
