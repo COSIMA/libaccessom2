84a370b53a4ffff8fc0a9f87f6804f055987fbb7
Nic Hannah <nichannah@pm.me>
2020-01-20 16:45:09 +1100

    Get test working on new machine. Why was Oasis accepting termination when there were hanging puts? Closes #38

--- tests/copy_test_data.sh
+++ tests/copy_test_data.sh
@@ -3,10 +3,10 @@
 mkdir -p test_data
 
 # Copy over OASIS remap weights
-cp /short/public/access-om2/input_rc/common_1deg_jra55/* test_data/
+cp /g/data/ik11/inputs/access-om2/input_rc/common_1deg_jra55/* test_data/
 
 # Copy over river runoff remap weights
-cp /short/public/access-om2/input_rc/yatm_1deg/rmp_jrar_to_cict_CONSERV.nc test_data/
+cp /g/data/ik11/inputs/access-om2/input_rc/yatm_1deg/rmp_jrar_to_cict_CONSERV.nc test_data/
 
 # Copy ice grid and OASIS restarts
-cp /short/public/access-om2/input_rc/cice_1deg/* test_data/
+cp /g/data/ik11/inputs/access-om2/input_rc/cice_1deg/* test_data/
