f906cc312ec7f1af0a6ffebc887ea010842756a4
Nic Hannah <nichannah@pm.me>
2021-04-29 14:37:39 +1000

    Simplify test parameterisation. #30

--- libforcing/test/test_forcing_perturbations.py
+++ libforcing/test/test_forcing_perturbations.py
@@ -22,10 +22,8 @@ forcing_tmpl = Template("""
       "fieldname": "{{forcing_fieldname}}",
       "cname": "{{coupling_fieldname}}",
       "pertubations": [
-        {{spatial}}
-        {{spatiotemporal}}
-        {{temporal}}
-        {{constant}}
+        {{pertubation0}}
+        {{pertubation1}}
       ]
     }
   ]
@@ -47,19 +45,28 @@ def get_forcing_field_shape():
 
 class TestForcingPerturbations:
 
-    def run_test(self, forcing_config, perturb_config):
+    def run_test(self, ptype, pdimension, pvalue, pcalendar):
 
-        forcing_file = forcing_config['inputs'][0]['filename']
-        fieldname = forcing_config['inputs'][0]['fieldname']
+        perturb_str = perturb_tmpl.render(type=ptype,
+                                          dimension=pdimension,
+                                          value=str(pvalue),
+                                          calendar=pcalendar)
+
+        with open('forcing.json', 'w') as f:
+            s = forcing_tmpl.render(forcing_filename=FORCING_FILE,
+                                    forcing_fieldname=FORCING_FIELDNAME,
+                                    coupling_fieldname=COUPLING_FIELDNAME,
+                                    pertubation0=perturb_str)
+            f.write(s)
 
         # Read out a random time point to test against
-        with nc.Dataset(forcing_file) as f:
+        with nc.Dataset(FORCING_FILE) as f:
             time_var = f.variables['time']
             times = nc.num2date(time_var[:], time_var.units)
             tidx = random.randint(0, len(times))
 
             date_str = times[tidx].strftime('%Y-%m-%dT%H:%M:%S')
-            src_data = f.variables[fieldname][tidx, :]
+            src_data = f.variables[FORCING_FIELDNAME][tidx, :]
 
         # Run fortran code with given datetime
         ret = sp.run(['./forcing_test.exe', date_str])
@@ -68,25 +75,33 @@ class TestForcingPerturbations:
         # Read fortran code output
         assert Path('test_output.nc').exists()
         with nc.Dataset('test_output.nc') as f:
-            dest_data = f.variables[fieldname][:]
+            dest_data = f.variables[FORCING_FIELDNAME][:]
 
         # Get the configured pertubation value
-        perturb_value = perturb_config['value']
-        if Path(perturb_value).exists():
-            with nc.Dataset(perturb_value) as f:
-                data_array = f.variables[fieldname][:]
+        if Path(str(pvalue)).exists():
+            with nc.Dataset(pvalue) as f:
+                perturb_array = f.variables[FORCING_FIELDNAME][:]
         else:
-            data_array = int(perturb_value)
+            perturb_array = int(pvalue)
 
         # Do the pertubation in Python code and check that it is as expected
-        if perturb_config['type'] == 'scaling':
-            assert np.allclose(src_data*data_array, dest_data)
+        if ptype == 'scaling':
+            assert np.allclose(src_data*perturb_array, dest_data)
         else:
-            assert np.allclose(src_data+data_array, dest_data)
-	
+            assert np.allclose(src_data+perturb_array, dest_data)
 
-    @pytest.mark.parametrize("perturbation_type", ['scaling', 'offset'])
-    def test_temporal(self, perturbation_type):
+
+    @pytest.mark.parametrize("perturb_type", ['scaling', 'offset'])
+    def test_spatiotemporal(self, perturb_type):
+        """
+        Test spatial scaling and offset
+        """
+
+        pass
+
+
+    @pytest.mark.parametrize("perturb_type", ['scaling', 'offset'])
+    def test_temporal(self, perturb_type):
         """
         Test temporal scaling and offset
         """
@@ -94,18 +109,9 @@ class TestForcingPerturbations:
         pass
 
 
-    @pytest.mark.parametrize("perturbation_type", ['scaling', 'offset'])
-    def test_spatiotemporal(self, perturbation_type):
-        """
-        Test spatial scaling and offset
-        """
 
-        pass
-
-
-
-    @pytest.mark.parametrize("perturbation_type", ['scaling', 'offset'])
-    def test_spatial(self, perturbation_type):
+    @pytest.mark.parametrize("perturb_type", ['scaling', 'offset'])
+    def test_spatial(self, perturb_type):
         """
         Test spatial scaling and offset
         """
@@ -113,47 +119,21 @@ class TestForcingPerturbations:
         # Create 2d pertubation file
         perturb_value = './test_input.nc'
 
-        perturb_str = perturb_tmpl.render(type=perturbation_type,
-                                          dimension='spatial',
-                                          value=str(perturb_value),
-                                          calendar='forcing')
-        with open('forcing.json', 'w') as f:
-            s = forcing_tmpl.render(forcing_filename=FORCING_FILE,
-                                    forcing_fieldname=FORCING_FIELDNAME,
-                                    coupling_fieldname=COUPLING_FIELDNAME,
-                                    spatial=perturb_str)
-            f.write(s)
-            forcing_config = json.loads(s)
- 
         shape =  get_forcing_field_shape()
         nx = shape[2]
         ny = shape[1]
         data_array = np.random.rand(ny, nx)
         create_nc_file(perturb_value, FORCING_FIELDNAME, data_array)
 
-        self.run_test(forcing_config, json.loads(perturb_str))
+        self.run_test(perturb_type, 'spatial', perturb_value, 'forcing')
 
 
-    @pytest.mark.parametrize("perturbation_type", ['scaling', 'offset'])
-    def test_constant(self, perturbation_type):
+    @pytest.mark.parametrize("perturb_type", ['scaling', 'offset'])
+    def test_constant(self, perturb_type):
         """
         Test constant scaling and offset
         """
 
         perturb_value = random.randint(0, 100)
-
-        perturb_str = perturb_tmpl.render(type=perturbation_type,
-                                          dimension='constant',
-                                          value=str(perturb_value),
-                                          calendar='forcing')
-
-        with open('forcing.json', 'w') as f:
-            s = forcing_tmpl.render(forcing_filename=FORCING_FILE,
-                                    forcing_fieldname=FORCING_FIELDNAME,
-                                    coupling_fieldname=COUPLING_FIELDNAME,
-                                    constant=perturb_str)
-            f.write(s)
-            forcing_config = json.loads(s)
- 
-        self.run_test(forcing_config, json.loads(perturb_str))
+        self.run_test(perturb_type, 'constant', perturb_value, 'forcing')
 
